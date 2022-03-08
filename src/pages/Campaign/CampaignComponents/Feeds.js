89867a7738f52221ae01243b911f4c736f22ef50
정지민 <rindalica@gmail.com>
2022-03-08 14:33:08 +0900

    Add : 피드 레이아웃 수정

--- src/pages/Campaign/CampaignComponents/Feeds.js
+++ src/pages/Campaign/CampaignComponents/Feeds.js
@@ -6,14 +6,19 @@ function Feeds() {
   return (
     <FeedsBox>
       <Feed feedWidth="300px" imgWidth="150px" imgHeight="150px" />
-      <SmallFeeds>
-        {/* TODO 
+      <SmallFeedsWrap>
+        <span>
+          캠페인 참여 게시물 순위 ((좋아요 + 댓글 수) 기준 / 매일 업데이트)
+        </span>
+        <SmallFeeds>
+          {/* TODO 
          map으로 구현 */}
-        <Feed feedWidth="240px" imgWidth="120px" imgHeight="120px" />
-        <Feed feedWidth="240px" imgWidth="120px" imgHeight="120px" />
-        <Feed feedWidth="240px" imgWidth="120px" imgHeight="120px" />
-        <Feed feedWidth="240px" imgWidth="120px" imgHeight="120px" />
-      </SmallFeeds>
+          <Feed feedWidth="240px" imgWidth="120px" imgHeight="120px" />
+          <Feed feedWidth="240px" imgWidth="120px" imgHeight="120px" />
+          <Feed feedWidth="240px" imgWidth="120px" imgHeight="120px" />
+          <Feed feedWidth="240px" imgWidth="120px" imgHeight="120px" />
+        </SmallFeeds>
+      </SmallFeedsWrap>
     </FeedsBox>
   );
 }
@@ -26,6 +31,12 @@ const FeedsBox = styled.div`
   margin-top: 15px;
   border-radius: 12px;
   border: 1px solid #e1e1ef;
+`;
+
+const SmallFeedsWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: end;
 `;
 
 const SmallFeeds = styled.div`
